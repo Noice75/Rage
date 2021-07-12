f9a86adfcac02a3544a3356f082304c14d75818e
Noice75 <84794374+Noice75@users.noreply.github.com>
2021-07-12 09:57:30 +0530

    Window Events
    Added Window Events

--- Rage/src/Rage/Application.cpp
+++ Rage/src/Rage/Application.cpp
@@ -1,21 +1,32 @@
 #include "rapch.h"
 #include "Application.h"
 
-#include "Rage/Events/ApplicationEvent.h"
 #include "Rage/Log.h"
 
 #include <GLFW/glfw3.h>
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 
 namespace Rage {
 
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		RA_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,6 +37,12 @@ namespace Rage {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Rage/src/Rage/Application.h
+++ Rage/src/Rage/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Rage/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Rage
@@ -14,7 +16,11 @@ namespace Rage
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
