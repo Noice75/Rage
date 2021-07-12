c068369fb4fecc09bc2513e277a009a999678875
Noice75 <84794374+Noice75@users.noreply.github.com>
2021-07-12 10:51:37 +0530

    imgui Events
    Added Iamgui Events

--- Rage/src/Rage/ImGui/ImGuiLayer.h
+++ Rage/src/Rage/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Rage/Layer.h"
 
+#include "Rage/Events/ApplicationEvent.h"
+#include "Rage/Events/KeyEvent.h"
+#include "Rage/Events/MouseEvent.h"
+
 namespace Rage {
 
 	class RAGE_API ImGuiLayer : public Layer
@@ -14,6 +18,17 @@ namespace Rage {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
