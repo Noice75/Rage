2076d0726847ac4195ab8f9c264c3e36cf232bc5
Noice75 <84794374+Noice75@users.noreply.github.com>
2021-07-12 09:48:11 +0530

    Premake
    Linked Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Rage"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Rage"
+	location "Rage"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RA_PLATFORM_WINDOWS",
+			"RA_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "RA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RA_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RA_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Rage/vendor/spdlog/include",
+		"Rage/src"
+	}
+
+	links
+	{
+		"Rage"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RA_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "RA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RA_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RA_DIST"
+		optimize "On"
