887fa99ad3b120c0acaf984c1087928c02453f00
Noice75 <84794374+Noice75@users.noreply.github.com>
2021-07-19 08:09:07 +0530

    Shader Abstraction and Uniforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,13 @@
 #include <Rage.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
 
 class ExampleLayer : public Rage::Layer
 {
@@ -89,9 +94,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Rage::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Rage::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -108,18 +113,21 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+
+            uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Rage::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Rage::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Rage::Timestep ts) override
@@ -154,13 +162,17 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Rage::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Rage::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Rage::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Rage::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -171,7 +183,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Rage::Event& event) override
@@ -183,7 +197,7 @@ private:
 	std::shared_ptr<Rage::Shader> m_Shader;
 	std::shared_ptr<Rage::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Rage::Shader> m_BlueShader;
+	std::shared_ptr<Rage::Shader> m_FlatColorShader;
 	std::shared_ptr<Rage::VertexArray> m_SquareVA;
 
 	Rage::OrthographicCamera m_Camera;
@@ -191,6 +205,8 @@ private:
 	float m_CameraMoveSpeed = 5.0f;
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 
