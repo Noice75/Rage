bf002cf2de4e2bbe7919a9fd31b6893ce0abe58d
Noice75 <84794374+Noice75@users.noreply.github.com>
2021-07-22 19:59:15 +0530

    Resizing

--- Rage/src/Rage/Application.cpp
+++ Rage/src/Rage/Application.cpp
@@ -51,6 +51,7 @@ namespace Rage {
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
 
 		RA_CORE_TRACE("{0}", e);
 
@@ -70,8 +71,11 @@ namespace Rage {
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate(timestep);
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
@@ -89,4 +93,17 @@ namespace Rage {
 		return true;
 	}
 
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
+	}
 }
