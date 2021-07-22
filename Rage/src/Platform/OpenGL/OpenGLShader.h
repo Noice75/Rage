d4fc09e8d18b66cf76f6cfd635eb8fe8c3fd218d
Noice75 <84794374+Noice75@users.noreply.github.com>
2021-07-22 08:40:45 +0530

    Shader Asset Files

--- Rage/src/Platform/OpenGL/OpenGLShader.h
+++ Rage/src/Platform/OpenGL/OpenGLShader.h
@@ -3,11 +3,15 @@
 #include "Rage/Renderer/Shader.h"
 #include <glm/glm.hpp>
 
+// TODO: REMOVE!
+typedef unsigned int GLenum;
+
 namespace Rage {
 
 	class OpenGLShader : public Shader
 	{
 	public:
+		OpenGLShader(const std::string& filepath);
 		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
@@ -23,6 +27,12 @@ namespace Rage {
 
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+
+	private:
+		std::string ReadFile(const std::string& filepath);
+		std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+		void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
+
 	private:
 		uint32_t m_RendererID;
 	};
--- Rage/src/Rage/Renderer/Shader.cpp
+++ Rage/src/Rage/Renderer/Shader.cpp
@@ -6,6 +6,19 @@
 
 namespace Rage {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    RA_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		}
+
+		RA_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
